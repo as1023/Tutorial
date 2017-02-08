266fe2f35c6158e7c34c51dae942f100dcd758af
github <amitbiochem04@gmail.com>
2017-02-08 23:48:37 +0100

    github.sh

---
+++ HIc_tut.sh
@@ -0,0 +1,12 @@
+
+https://genomebiology.biomedcentral.com/articles/10.1186/s13059-015-0831-x
+http://www.sciencedirect.com/science/article/pii/S1046202314003582
+http://www.bioconductor.org/packages/release/bioc/html/HiTC.html
+https://www.ebi.ac.uk/sites/ebi.ac.uk/files/shared/documents/phdtheses/RobertThesis_2014-12-12_v03_CORRECTED.pdf
+http://chromosome.sdsc.edu/mouse/hi-c/download.html
+http://www.nature.com/ncomms/2015/150219/ncomms7178/full/ncomms7178.html
+https://bioconductor.org/packages/3.3/bioc/vignettes/diffHic/inst/doc/diffHicUsersGuide.pdf
+https://github.com/theaidenlab/juicer
+http://journals.plos.org/ploscompbiol/article?id=10.1371/journal.pcbi.1004221
+http://my5c.umassmed.edu/welcome/welcome.php
+https://github.com/dekkerlab
---
+++ github.sh
@@ -0,0 +1,16 @@
+
+###to add subfolder then following comand 
+##fisrt of download github and install
+
+##to add subfolder then following comand
+
+##click creatnewfile from github to have subfolder 
+  
+git pull origin 
+
+git add .
+git commit -m "creat"
+git push origin master
+
+
+
